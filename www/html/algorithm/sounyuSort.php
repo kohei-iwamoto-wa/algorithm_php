c28ae8813b58720ec9554d5823977c843fb2aaf9
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-05-02 22:09:21 +0900

    コメント追加

--- www/html/algorithm/sounyuSort.php
+++ www/html/algorithm/sounyuSort.php
@@ -4,9 +4,13 @@ function sounyuSort(&$nums) {
     // $nums[0]以降の要素ををループで処理する
     for ($i = 1;$i<=count($nums) - 1;$i++) {
         for ($j = $i; $j >= 1; $j--) {
+            // 現在の要素より直前の要素が大きい場合
             if ($nums[$j - 1] > $nums[$j]) {
+                // 一次変数に対比
                 $temp = $nums[$j - 1];
+                // 値をスライド
                 $nums[$j - 1] = $nums[$j];
+                // 一次変数に格納した値をスライド
                 $nums[$j] = $temp;
             } else {
                 break;
