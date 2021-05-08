77a817136c85695a0d256b083916e500e487fada
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-05-08 21:33:52 +0900

    99追加

--- www/html/99.php
+++ www/html/99.php
@@ -1,9 +1,35 @@
 <?php
 // 九九表
-
 for ($i = 1; $i <= 9; $i++) {
     for ($j = 1; $j <= 9; $j++) {
         echo $i * $j;
+    }
+    echo PHP_EOL;
+}
+
+echo '<br>';
+// 逆
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        echo (10 - $i) * (10 - $j);
+    }
+    echo PHP_EOL;
+}
+
+echo '<br>';
+
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        echo $i * (10 - $j);
+    }
+    echo PHP_EOL;
+}
+
+echo '<br>';
+
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        echo (10 - $i) * $j;
     }
     echo PHP_EOL;
 }
